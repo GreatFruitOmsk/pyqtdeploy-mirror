f4c6a2b54272bdd54f5a8f5f0caa1d6d13eaea5f
Phil Thompson <phil@riverbankcomputing.com>
2014-03-19 09:09:01 +0000

    Switched to the 2 clause BSD license.

--- pyqtdeploy/__init__.py
+++ pyqtdeploy/__init__.py
@@ -1,16 +1,27 @@
-# Copyright (c) 2014 Riverbank Computing Limited.
+# Copyright (c) 2014, Riverbank Computing Limited
+# All rights reserved.
 #
-# This file is part of pyqtdeploy.
-#
-# This file may be used under the terms of the GNU General Public License
-# v2 or v3 as published by the Free Software Foundation which can be found in
-# the files LICENSE-GPL2.txt and LICENSE-GPL3.txt included in this package.
-# In addition, as a special exception, Riverbank gives you certain additional
-# rights.  These rights are described in the Riverbank GPL Exception, which
-# can be found in the file GPL-Exception.txt in this package.
-#
-# This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-# WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 
 # Publish the package's API.
