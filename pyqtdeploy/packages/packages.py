3da6e386382badebcff7ec2150d40b6e205cb6aa
Phil Thompson <phil@riverbankcomputing.com>
2014-06-12 18:27:40 +0100

    Implemented the configure option for supported packages.

--- pyqtdeploy/packages/packages.py
+++ pyqtdeploy/packages/packages.py
@@ -24,14 +24,86 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 
+import sys
+
 from PyQt5.QtCore import QDir, QFile, QFileInfo, QIODevice
 
 from ..user_exception import UserException
 
 
-def configure_package(package, target, output):
+# The mapping of supported targets to Python platforms.
+_TARGET_PYPLATFORM_MAP = {
+    'linux':    'linux',
+    'win':      'win32',
+    'osx':      'darwin',
+    'ios':      'darwin',
+    'android':  'linux'
+}
 
-    print("Generating %s-%s.cfg as %s" % (package, target, output))
+
+def configure_package(package, target, output):
+    """ Write a configuration file for a particular package and target. """
+
+    # Get the package directory and check it is valid.
+    package_qdir = _config_qdir()
+
+    if not package_qdir.cd(package):
+        raise UserException("{0} is not a supported package.".format(package))
+
+    # Get the target platform.
+    if target is None:
+        # Default to the host platform.
+        if sys.platform.startswith('linux'):
+            target = 'linux'
+        elif sys.platform == 'win32':
+            target = 'win'
+        elif sys.platform == 'darwin':
+            target = 'osx'
+
+    try:
+        py_platform = _TARGET_PYPLATFORM_MAP[target]
+    except KeyError:
+        raise UserException("{0} is not a supported target.".format(target))
+
+    # Make sure we have a name to write to.
+    if output is None:
+        output = _config_name(package, target)
+
+    # See if there is a target-specific file.
+    name = _config_name(package, target)
+    if not package_qdir.exists(name):
+        name = _config_name(package)
+        if not package_qdir.exists(name):
+            raise UserException("Internal error - no configuration file.")
+
+    # Read the configuration file.
+    config_qfile = QFile(package_qdir.absoluteFilePath(name))
+
+    if not config_qfile.open(QIODevice.ReadOnly|QIODevice.Text):
+        raise UserException(
+                "Unable to open file {0}.".format(config_qfile.fileName()),
+                config_qfile.errorString())
+
+    config = config_qfile.readAll()
+    config_qfile.close()
+
+    # Expand any macros.
+    config.replace('@PY_PLATFORM@', py_platform)
+
+    # Write the output.
+    output_qfile = QFile(output)
+
+    if not output_qfile.open(QIODevice.WriteOnly|QIODevice.Text):
+        raise UserException(
+                "Unable to create file {0}.".format(output_qfile.fileName()),
+                output_qfile.errorString())
+
+    if output_qfile.write(config) < 0:
+        raise UserException(
+                "Unable to write to file {0}.".format(output_qfile.fileName()),
+                output_qfile.errorString())
+
+    output_qfile.close()
 
 
 def show_packages():
@@ -44,6 +116,17 @@ def show_packages():
 
     for package in packages:
         print(package)
+
+
+def _config_name(package, target=None):
+    """ Return the name of a configuration file for a platform and optional
+    target.
+    """
+
+    if target is None:
+        return '{0}.cfg'.format(package)
+
+    return '{0}-{1}.cfg'.format(package, target)
 
 
 def _config_qdir():
