7e0c7f43a72e89d8bdeb218cd151cb0335c7aa1c
Phil Thompson <phil@riverbankcomputing.com>
2016-01-04 18:46:53 +0000

    Updates to the developer notes regarding iOS deployment.

--- demo/build.sh
+++ demo/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Build for all
 export SYSROOT=$HOME/usr/sysroot
 rm -rf build
 pyqtdeploycli --project pyqt-demo.pdy build
@@ -7,8 +8,13 @@ cd build
 $SYSROOT/qt-5.5.1/bin/qmake
 make
 
+# Run for OSX.
+./pyqt-demo.app/Contents/MacOS/pyqt-demo
+
+# Run for Linux.
+#./pyqt-demo
+
+# Deploy for Android.
 #make install INSTALL_ROOT=deploy
 #$SYSROOT/qt-5.5.1/bin/androiddeployqt --input android-libpyqt-demo.so-deployment-settings.json --output deploy
 #adb install deploy/bin/QtApp-debug.apk
-
-./pyqt-demo.app/Contents/MacOS/pyqt-demo
