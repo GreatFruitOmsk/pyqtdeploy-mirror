0a3b0438249ce8687c9bf955e30db8c49068aba1
Phil Thompson <phil@riverbankcomputing.com>
2016-08-12 17:50:33 +0100

    Migrated from the old to the new build system.

--- doc/conf.py
+++ doc/conf.py
@@ -70,7 +70,7 @@ version = release.split('-')[0]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['html']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
