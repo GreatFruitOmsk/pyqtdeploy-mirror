733155d7fe07a2a12c15c317191237804076746f
Phil Thompson <phil@riverbankcomputing.com>
2016-08-23 12:56:35 +0100

    Updated the developer notes.

--- demo/build.bat
+++ demo/build.bat
@@ -1,6 +1,6 @@
 set SYSROOT=C:\sysroot-win-64
 if exist build rmdir /s /q build
-c:\Python35\Scripts\pyqtdeploycli --project pyqt-demo.pdy build
+pyqtdeploycli --project pyqt-demo.pdy build
 chdir build
 %SYSROOT%\bin\qmake
 nmake
