598935c04447714170f616b32ef5b0b91bf944f0
Phil Thompson <phil@riverbankcomputing.com>
2017-09-19 10:00:23 +0100

    Added the PyQt3D package plugin.

---
+++ pyqtdeploy/sysroot/packages/pyqt3d.py
@@ -0,0 +1,76 @@
+# Copyright (c) 2017, Riverbank Computing Limited
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+import os
+
+from ... import AbstractPackage, PackageOption
+
+
+class PyQt3DPackage(AbstractPackage):
+    """ The PyQt3D package. """
+
+    # The package-specific options.
+    options = [
+        PackageOption('source', str, required=True,
+                help="The archive containing the PyQt3D source code."),
+    ]
+
+    def build(self, sysroot):
+        """ Build PyQt3D for the target. """
+
+        sysroot.progress("Building PyQt3D")
+
+        # Unpack the source.
+        archive = sysroot.find_file(self.source)
+        sysroot.unpack_archive(archive)
+
+        # Create a configuration file.
+        cfg = '''py_inc_dir = {0}
+py_pylib_dir = {1}
+py_pylib_lib = {2}
+py_sip_dir = {3}
+[PyQt 5]
+module_dir = {4}
+'''.format(sysroot.target_py_include_dir, sysroot.target_lib_dir,
+                sysroot.target_py_lib, sysroot.target_sip_dir,
+                os.path.join(sysroot.target_sitepackages_dir, 'PyQt5'))
+
+        if sysroot.pyqt5_disabled_features:
+                cfg += 'pyqt_disabled_features = {0}\n'.format(
+                        ' '.join(sysroot.pyqt5_disabled_features))
+
+        cfg_name = 'pyqt3d-' + sysroot.target_name + '.cfg'
+
+        with open(cfg_name, 'wt') as cfg_file:
+            cfg_file.write(cfg)
+
+        # Configure, build and install.
+        sysroot.run(sysroot.host_python, 'configure.py', '--static', '--qmake',
+            sysroot.host_qmake, '--sysroot', sysroot.sysroot_dir,
+            '--no-qsci-api', '--no-sip-files', '--no-stubs', '--configuration',
+            cfg_name, '--sip', sysroot.host_sip, '-c')
+        sysroot.run(sysroot.host_make)
+        sysroot.run(sysroot.host_make, 'install')
