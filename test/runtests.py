70f1371097f69d8e861cee8bc302a95bae339b87
Phil Thompson <phil@riverbankcomputing.com>
2019-03-09 23:27:09 +0000

    Refactored the test harnes so that tests can be organised by component and to be able to run all the tests for a component in one invocation. A test directory may contain an 'Ignore' file which specifies tests to ignore on a per-platform basis.  This means tests don't have to be repeated in target-specific directories.

--- test/runtests.py
+++ test/runtests.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import glob
 import os
 import shutil
 import subprocess
@@ -11,38 +10,91 @@ class UserException(Exception):
     """ An exception used for reporting user-triggered errors. """
 
 
-class TargetTests:
-    """ Encapsulate a set of tests for a particular target. """
+def default_target():
+    """ Return the default target. """
 
-    def __init__(self, target, test=None):
+    if sys.platform.startswith('linux'):
+        target = 'linux-64'
+    elif sys.platform == 'win32':
+        # MSVC2015 is v14, MSVC2017 is v15.
+        vs_major = os.environ.get('VisualStudioVersion', '0.0').split('.')[0]
+
+        if vs_major == '15':
+            is_32 = (os.environ.get('VSCMD_ARG_TGT_ARCH') != 'x64')
+        elif vs_major == '14':
+            is_32 = (os.environ.get('Platform') != 'X64')
+        else:
+            # Default to 64 bits.
+            is_32 = False
+
+        target = 'win-' + ('32' if is_32 else '64')
+    elif sys.platform == 'darwin':
+        target = 'macos-64'
+    else:
+        raise UserException("unsupported host platform")
+
+    return target
+
+
+def find_tests(test, target):
+    """ Return the list of test files for a target. """
+
+    tests = []
+
+    if test is None:
+        test = 'tests'
+    elif not os.path.exists(test):
+        raise UserException("'{0}' does not exist".format(test))
+
+    if os.path.isfile(test):
+        tests.append(test)
+    else:
+        for dirpath, _, filenames in os.walk(test):
+            # See if there is a file describing the tests to ignore for a
+            # particular platform.
+            ignore = {}
+
+            ignore_file = os.path.join(dirpath, 'Ignore')
+            if os.path.isfile(ignore_file):
+                with open(ignore_file) as ig_f:
+                    for line in ig_f:
+                        line = line.strip()
+
+                        # Ignore comments.
+                        if line.startswith('#'):
+                            continue
+
+                        # We just ignore bad lines.
+                        parts = line.split(':')
+                        if len(parts) != 2:
+                            continue
+
+                        test_file = parts[0].strip()
+                        if test_file == '':
+                            continue
+
+                        ignore[test_file] = parts[1].strip.split()
+
+            for fn in filenames:
+                if target in ignore.get(fn, []):
+                    continue
+
+                tests.append(os.path.join(dirpath, fn))
+
+    return tests
+
+
+class TestCase:
+    """ Encapsulate a test for a particular target. """
+
+    def __init__(self, test, target):
         """ Initialise the object. """
 
-        if not target:
-            if sys.platform.startswith('linux'):
-                target = 'linux-64'
-            elif sys.platform == 'win32':
-                # MSVC2015 is v14, MSVC2017 is v15.
-                vs_major = os.environ.get('VisualStudioVersion', '0.0').split('.')[0]
-
-                if vs_major == '15':
-                    is_32 = (os.environ.get('VSCMD_ARG_TGT_ARCH') != 'x64')
-                elif vs_major == '14':
-                    is_32 = (os.environ.get('Platform') != 'X64')
-                else:
-                    # Default to 64 bits.
-                    is_32 = False
-
-                target = 'win-' + ('32' if is_32 else '64')
-            elif sys.platform == 'darwin':
-                target = 'macos-64'
-            else:
-                raise UserException("unsupported host platform")
-
+        self.test = test
         self.target = target
 
-        self._tests = [test] if test else self._find_tests()
-
-    def call(self, args, verbose, failure_message):
+    @staticmethod
+    def call(args, verbose, failure_message):
         """ Call a sub-process. """
 
         if verbose:
@@ -82,59 +134,25 @@ class TargetTests:
         if failed:
             raise UserException(failure_message)
 
-    @classmethod
-    def factory(cls, target, test):
-        """ Create a tests instance for a particular test. """
-
-        if test.endswith(TargetSysrootTests.test_extension):
-            test_type = TargetSysrootTests
-        elif test.endswith(TargetStdlibTests.test_extension):
-            test_type = TargetStdlibTests
-        else:
-            raise UserException("unknown test type: {0}".format(test))
-
-        return test_type(target, os.path.abspath(test))
-
     def run(self, source_dirs, no_clean, verbose):
-        """ Run the tests. """
-
-        for test in self._tests:
-            self.run_test(test, source_dirs, no_clean, verbose)
-
-    def run_test(self, test, source_dirs, no_clean, verbose):
-        """ Re-implemented to run a single test. """
+        """ Re-implemented to run a test. """
 
         raise NotImplementedError
 
-    def _find_tests(self):
-        """ Return the sequence of test files. """
 
-        # Search the supported target directories.
-        tests = []
-
-        for target_dir in [self.target, 'common']:
-            tests.extend(
-                    glob.glob(
-                            os.path.join('tests', target_dir,
-                                    '*' + self.test_extension)))
-
-        return tests
-
-
-class TargetSysrootTests(TargetTests):
-    """ Encapsulate a set of pyqtdeploy-sysroot tests for a particular target.
-    """
+class SysrootTest(TestCase):
+    """ Encapsulate a pyqtdeploy-sysroot test for a particular target. """
 
     # The filename exyension of pyqtdeploy-sysroot tests.
     test_extension = '.json'
 
-    def run_test(self, test, source_dirs, no_clean, verbose):
+    def run(self, source_dirs, no_clean, verbose):
         """ Run a pyqtdeploy-sysroot test. """
 
-        print("Building sysroot from {}".format(test))
+        print("Building sysroot from {}".format(self.test))
 
         # The name of the sysroot directory to be built.
-        test_name = os.path.basename(test).split('.')[0]
+        test_name = os.path.basename(self.test).split('.')[0]
         sysroot = os.path.join('sysroot',
                 '{0}-{1}'.format(self.target, test_name))
 
@@ -152,25 +170,24 @@ class TargetSysrootTests(TargetTests):
 
         args.extend(['--target', self.target])
         args.extend(['--sysroot', sysroot])
-        args.append(test)
+        args.append(self.test)
 
         self.call(args, verbose,
-                "Build of sysroot from {} failed".format(test))
+                "Build of sysroot from {} failed".format(self.test))
 
-        print("Build of sysroot from {} successful".format(test))
+        print("Build of sysroot from {} successful".format(self.test))
 
 
-class TargetStdlibTests(TargetTests):
-    """ Encapsulate a set of pyqtdeploy-build tests for a particular target.
-    """
+class StdlibTest(TestCase):
+    """ Encapsulate a pyqtdeploy-build test for a particular target. """
 
     # The filename exyension of pyqtdeploy-build tests.
     test_extension = '.pdy'
 
-    def run_test(self, test, source_dirs, no_clean, verbose):
+    def run(self, source_dirs, no_clean, verbose):
         """ Run a pyqtdeploy-build test. """
 
-        print("Building application from {}".format(test))
+        print("Building application from {}".format(self.test))
 
         # The name of the sysroot directory to use.
         sysroot = os.path.join('sysroot',
@@ -187,10 +204,10 @@ class TargetStdlibTests(TargetTests):
 
         args.extend(['--target', self.target])
         args.extend(['--sysroot', sysroot])
-        args.append(test)
+        args.append(self.test)
 
         self.call(args, verbose,
-                "pyqtdeploy-build using {} failed".format(test))
+                "pyqtdeploy-build using {} failed".format(self.test))
 
         # Change to the build directory and run qmake and make.
         build_dir = 'build-' + self.target
@@ -205,7 +222,7 @@ class TargetStdlibTests(TargetTests):
         if not no_clean:
             shutil.rmtree(build_dir)
 
-        print("Build of application from {} successful".format(test))
+        print("Build of application from {} successful".format(self.test))
 
 
 if __name__ == '__main__':
@@ -222,7 +239,7 @@ if __name__ == '__main__':
             help="a directory containing the source archives",
             metavar='DIR', dest='source_dirs', action='append')
     parser.add_argument('--test',
-            help="the JSON specification file or project file")
+            help="a test directory, a JSON specification file or project file")
     parser.add_argument('--target', help="the target platform")
     parser.add_argument('--verbose', help="enable verbose progress messages",
             action='store_true')
@@ -234,23 +251,37 @@ if __name__ == '__main__':
     else:
         source_dirs = None
 
+    if args.test:
+        test = os.path.abspath(args.test)
+    else:
+        test = None
+
     # Anchor everything from the directory containing this script.
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     if not source_dirs:
         source_dirs = [os.path.join('..', 'demo', 'src')]
 
-    # Run a specific test or all of them.
+    # Run the tests.
     try:
-        if args.test:
-            TargetTests.factory(args.target, args.test).run(source_dirs,
-                    args.no_clean, args.verbose)
+        if args.target:
+            target = args.target
         else:
-            # The sysroot tests must be run first.
-            TargetSysrootTests(args.target).run(source_dirs, args.no_clean,
-                    args.verbose)
-            TargetStdlibTests(args.target).run(source_dirs, args.no_clean,
-                    args.verbose)
+            target = default_target()
+
+        tests = find_tests(test, target)
+
+        # Any sysroot tests must be run first.
+        for test_case in tests:
+            if test.endswith(SysrootTest.test_extension):
+                SysrootTest(test, target).run(source_dirs, args.no_clean,
+                        args.verbose)
+
+        for test_case in tests:
+            if test.endswith(StdlibTest.test_extension):
+                StdlibTest(test, target).run(source_dirs, args.no_clean,
+                        args.verbose)
+
     except UserException as e:
         print(e)
         sys.exit(1)
